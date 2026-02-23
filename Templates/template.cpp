f02efa6ac8a6088784b57b8c4f9d1e28becb0c0d
profalaxiz <134207210+profalaxiz@users.noreply.github.com>
2026-02-23 09:18:19 +0800

    First Template

---
+++ Templates/template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n;
+    // cin >> n;
+    
+    
+    
+    // return;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
