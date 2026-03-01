7d6022cbfe5eb42fb90e45273cb87cb0d50e5a4e
profalaxiz <134207210+profalaxiz@users.noreply.github.com>
2026-03-01 23:25:31 +0800

    Added 1 problem from CSES Introductory Problems

---
+++ CSES/Introductory Problems/cses1754_CoinPiles.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    
+    ll a, b; cin >> a >> b;
+
+    if ((a + b) % 3 == 0 && max(a, b) <= 2 * min(a, b)) cout << "YES" << '\n';
+    else cout << "NO" << '\n';
+    
+    return;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
