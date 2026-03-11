2a31efaa8fce683bbe5ed5df6c530d24ed45e13f
profalaxiz <134207210+profalaxiz@users.noreply.github.com>
2026-03-11 23:52:22 +0800

    Added 1 Codeforces 800 rated String Problem

---
+++ Codeforces/800-1199/cf71_WayTooLongWords.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+
+// Types & Containers
+using ll = long long;
+using str = string;
+using vi = vector<int>;
+using pi = pair<int,int>;
+using vpi = vector<pi>;
+using si = set<int>;
+
+// Macros
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define rev(x) reverse(all(x))
+#define del(x, i) erase(begin(x)+i)
+#define rem(x,i) erase(begin(x), begin(x)+i)
+#define mp make_pair
+
+// Constants
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+#define each_perm(x) sort(all(x)); do
+#define end_perm(x) while(next_permutation(all(x))) 
+
+// Loops
+#define Trav(a,x) for (auto& a: x)
+#define For(i,a,b) for (int i = (a); i < (b); ++i)
+
+void setIO(string name = "") {
+    
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    if (s.length() <= 10) cout << s << '\n';
+    else
+    {
+        string ans = "";
+        ans += s[0];
+        ans += to_string(s.length() - 2);
+        ans += s[s.length() - 1];
+        cout << ans << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // setIO("");
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    auto end_time = chrono::high_resolution_clock::now();
+    #ifndef ONLINE_JUDGE
+        chrono::duration<double, milli> duration = end_time - start_time;
+        cerr << "\n--- Execution Time: " << fixed << setprecision(2) << duration.count() << " ms ---" << endl;
+    #endif
+    return 0;
+}
