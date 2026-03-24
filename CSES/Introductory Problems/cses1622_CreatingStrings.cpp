6bb0fc4764f4fce7f3f2b4ef4cd235ccfd99ff1c
profalaxiz <134207210+profalaxiz@users.noreply.github.com>
2026-03-24 23:31:55 +0800

    Added 1 CSES introductory problem

---
+++ CSES/Introductory Problems/cses1622_CreatingStrings.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+
+// Types & Containers
+using ll = long long;
+using str = string;
+using vi = vector<int>;
+using pi = pair<int,int>;
+using vpi = vector<pi>;
+using si = set<int>;
+
+// Macros
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define rev(x) reverse(all(x))
+#define del(x, i) erase(begin(x)+i)
+#define rem(x,i) erase(begin(x), begin(x)+i)
+#define mp make_pair
+
+// Constants
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+#define each_perm(x) sort(all(x)); do
+#define end_perm(x) while(next_permutation(all(x))) 
+
+// Loops
+#define Trav(a,x) for (auto& a: x)
+#define For(i,a,b) for (int i = (a); i < (b); ++i)
+
+void setIO(string name = "") {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+}
+
+void solve() {
+    string s;
+    if (cin >> s) {
+        sort(s.begin(), s.end());
+        
+        vector<string> results;
+        
+        do {
+            results.push_back(s);
+        } while (next_permutation(s.begin(), s.end()));
+        
+        cout << results.size() << "\n";
+        
+        for (const string& str : results) {
+            cout << str << "\n";
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // setIO("");
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    auto end_time = chrono::high_resolution_clock::now();
+    #ifndef ONLINE_JUDGE
+        chrono::duration<double, milli> duration = end_time - start_time;
+        cerr << "\n--- Execution Time: " << fixed << setprecision(2) << duration.count() << " ms ---" << endl;
+    #endif
+    return 0;
+}
