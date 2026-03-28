95aa323f78508a07e3b26232b539f56b0500cd81
profalaxiz <134207210+profalaxiz@users.noreply.github.com>
2026-03-28 23:54:32 +0800

    Added 1 CSES sorting and searching problem

---
+++ CSES/Sorting and Searching/cses1640_SumOfTwoValues.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+
+// Types & Containers
+using ll = long long;
+using str = string;
+using vi = vector<int>;
+using pi = pair<int,int>;
+using vpi = vector<pi>;
+using si = set<int>;
+
+// Macros
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define rev(x) reverse(all(x))
+#define del(x, i) erase(begin(x)+i)
+#define rem(x,i) erase(begin(x), begin(x)+i)
+#define mp make_pair
+
+// Constants
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+#define each_perm(x) sort(all(x)); do
+#define end_perm(x) while(next_permutation(all(x))) 
+
+// Loops
+#define Trav(a,x) for (auto& a: x)
+#define For(i,a,b) for (int i = (a); i < (b); ++i)
+
+void setIO(string name = "") {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+}
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+
+    vpi arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i].first;
+        arr[i].second = i + 1; 
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+        ll sum = (ll)arr[left].first + arr[right].first;
+        
+        if (sum == x) {
+            cout << min(arr[left].second, arr[right].second) << " " 
+                << max(arr[left].second, arr[right].second) << "\n";
+            return;
+        } else if (sum < x) {
+            left++;  
+        } else {
+            right--; 
+        }
+    }
+
+    cout << "IMPOSSIBLE\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // setIO("");
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    auto end_time = chrono::high_resolution_clock::now();
+    #ifndef ONLINE_JUDGE
+        chrono::duration<double, milli> duration = end_time - start_time;
+        cerr << "\n--- Execution Time: " << fixed << setprecision(2) << duration.count() << " ms ---" << endl;
+    #endif
+    return 0;
+}
